d679ac628197c401eebd6ff9103ab9739a87a474
jaeminjo732 <jaeminjo732@gmail.com>
2025-12-15 17:07:12 +0900

    chore :: 글쓰기 페이지 인증 우회
    세션 체크와 관리자 이메일 검사 없이 글쓰기 클라이언트만 렌더링하도록 수정했습니다.

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,17 +1,5 @@
-import { auth } from "@/auth";
-import { redirect } from "next/navigation";
 import { WritePageClient } from "./write-page-client";
 
 export default async function WritePage() {
-    const session = await auth();
-
-    if (!session?.user) {
-        redirect("/api/auth/signin");
-    }
-
-    if (session.user.email !== process.env.ADMIN_EMAIL) {
-        redirect("/");
-    }
-
-    return <WritePageClient user={session.user} />;
+    return <WritePageClient user={{}} />;
 }
